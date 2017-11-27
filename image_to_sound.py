8ef9405f7650c4ba87b4cfb9204699cee4ddea87
Siddharth Nayak <siddharthnayak98@gmail.com>
2017-11-28 02:44:57 +0530

    Create image_to_sound.py

---
+++ image_to_sound.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.image as mpimg
+import wave
+from array import array
+
+
+def make_wav(image_filename):
+    """ Make a WAV file having a spectrogram resembling an image """
+    # Load image
+    image = mpimg.imread(image_filename)
+    image = np.sum(image, axis = 2).T[:, ::-1]
+    image = image**3 # ???
+    w, h = image.shape
+
+    # Fourier transform, normalize, remove DC bias
+    data = np.fft.irfft(image, h*2, axis=1).reshape((w*h*2))
+    data -= np.average(data)
+    data *= (2**15-1.)/np.amax(data)
+    data = array("h", np.int_(data)).tostring()
+
+    # Write to disk
+    output_file = wave.open(image_filename+".wav", "w")
+    output_file.setparams((1, 2, 44100, 0, "NONE", "not compressed"))
+    output_file.writeframes(data)
+    output_file.close()
+    print ("Wrote %s.wav" % image_filename)
+
+
+make_wav(my_image)
